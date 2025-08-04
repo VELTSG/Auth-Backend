0b14e8fd8b727a85d01089cdc2ccbcdae44afcd1
Kiruvel Tsegaye <Veltsegaye@gmail.com>
2025-08-04 14:25:14 +0300

    installed jwt and cookie-parser and imported into server js

--- client/js/login.js
+++ client/js/login.js
@@ -1,4 +1,5 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    // DOMMANUPULATION
   e.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
 const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
@@ -9,6 +11,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(express.json()); // Parse JSON bodies
 
 // Connect to MongoDB
