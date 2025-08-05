1531646b0177229d4a06877c0d6478ab990bce2b
Kiruvel Tsegaye <Veltsegaye@gmail.com>
2025-08-05 11:05:15 +0300

    removed the console logs on terminal as it got annoying

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log('Token:', token);
+  // console.log('Token:', token);
 
   if (!token) return res.status(401).json({ message: 'Unauthorized: No token' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('Decoded user:', decoded);
+    // console.log('Decoded user:', decoded);
     req.user = decoded; // You can access req.user in your protected routes
     next();
   } catch (err) {
